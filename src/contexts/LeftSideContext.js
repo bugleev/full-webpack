0008671976d8f2d781a419246846f4d11e600be2
Andrey Bugleev <andrey.bugleev@dataart.com>
2019-04-18 10:55:23 +0300

    added context compose

--- src/contexts/LeftSideContext.js
+++ src/contexts/LeftSideContext.js
@@ -1,32 +1,50 @@
 import React, { Component } from 'react'
 let MyContext
 const { Provider, Consumer } = MyContext = React.createContext();
+
+function ProviderComposer({ contexts, children }) {
+  return contexts.reduceRight(
+    (kids, parent) => React.cloneElement(parent, {
+      children: kids
+    }),
+    children
+  );
+}
+
+function ContextsProvider({ children }) {
+  return (
+    <ProviderComposer contexts={[<Provider />]}>
+      {children}
+    </ProviderComposer >
+  );
+}
+
 class LeftSideProvider extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       leftCounter: 0,
       rightCounter: 0,
       incrementLeft: this.incrementLeft,
       incrementRight: this.incrementRight
     };
-   
+
   }
-  
- 
-  incrementLeft=()=>{
-    this.setState({leftCounter: this.state.leftCounter + 1})
+
+
+  incrementLeft = () => {
+    this.setState({ leftCounter: this.state.leftCounter + 1 })
   }
-  incrementRight=()=>{
-    this.setState({rightCounter: this.state.rightCounter + 1})
+  incrementRight = () => {
+    this.setState({ rightCounter: this.state.rightCounter + 1 })
   }
-  render() {   
+  render() {
     return (
-     <Provider value={this.state}>
-       {this.props.children}
-     </Provider>
+      <Provider value={this.state}>
+        {this.props.children}
+      </Provider>
     )
   }
 }
 
-export { LeftSideProvider, Consumer, MyContext};
+export { LeftSideProvider, Consumer, MyContext };
